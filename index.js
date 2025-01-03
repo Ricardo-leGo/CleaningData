df37ae77a2ce8a4b449b65ea0ba1d77526d5f507
RICARDO LEYVA <A8046854@banorte.com>
2025-01-03 14:38:00 -0600

    sql files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ const ASss  = fs.readFile(process.env.Archivo, 'utf8', async (err, data) => {
     console.error('Error al leer el archivo:', err);
     return;
   }
+
   const Datafup = JSON.parse(data);
 
   let Keys = '';
@@ -16,17 +18,14 @@ const ASss  = fs.readFile(process.env.Archivo, 'utf8', async (err, data) => {
 INSERT INTO FupSections (Name, [Active]) VALUES ('${el}', 1);
 `
 );
-
 const AllQuestions =  [];
-
  const cleaneddata =  Object.keys(Datafup)
     .map(el => ({"Section":el,arrs: Datafup[el].flat()}))
     .map(el => ({
         Section:el.Section, 
         arrs:el.arrs.map((k, index)=> Object.keys(k).map((n) => ({ 
             IdArcher:k[n].Id, 
-            Question:k[n], 
-            DisplayName:n, 
+            Question:{...k[n], DisplayName:n},  
             Order:index, 
             Active:true}) ))
             .flat()
@@ -36,12 +35,13 @@ const AllQuestions =  [];
   
       AllQuestions.push (
         {
+          Display:kk.Question.DisplayName,
           IdArcher:kk.Question.Id,
+          LevelId:kk.Question.LevelId,
           Alias:kk.Question.Alias,
           TypeText:kk.Question.TypeText,
           Type:kk.Question.Type,
       
-          LevelId:kk.Question.LevelId,
           ...(kk.Question.RelatedValuesListId? {RelatedValuesListId: Object.keys(kk.Question.RelatedValuesListId).map( R => kk.Question.RelatedValuesListId[R] )}: {} ),
           ...(kk.Question.SubformData? {SubformFieldId: kk.Question.SubformFieldId, SubformData: kk.Question.SubformData.map(m =>  Object.values(m)[0].Id)  } : {} )
         }
@@ -83,7 +83,6 @@ const AllQuestions =  [];
     
     const RelatedValuesListIdCleaned =  cleaneddata.map( el => (
       { Section: el.Section, 
-        
         arrs: el.arrs.map( R => ({...R, Question:{...R.Question, 
           ...(R.Question.RelatedValuesListId? {RelatedValuesListId:R.Question.RelatedValuesListId.map(I =>{ return  I.IdArcher })}:{}  ),
           ...(R.Question.SubformData? {SubformData:R.Question.SubformData.map(I =>{ return Object.values(I)[0].Id})}:{}  ),
@@ -95,26 +94,118 @@ const AllQuestions =  [];
        el.arrs.forEach( ({Question}) =>  {
         Question.SubformData.forEach( S => {
           Object.keys(S).forEach( SQ => {
+            
+            const IdArcher = S[SQ].Id;
+            delete S[SQ].Id;
+
             SubformsQuests.push(
                 {
                   Display:SQ,
-                  ...S[SQ]
+                  IdArcher,
+                  ...S[SQ],
+                  ...(S[SQ].RelatedValuesListId? {RelatedValuesListId:Object.values(S[SQ].RelatedValuesListId).map(I =>{ return  I })}:{}  )
+
                 }
             );
+
+              if( S[SQ].RelatedValuesListId ){
+
+                  Object.keys(S[SQ].RelatedValuesListId).map( m => m).forEach( n => {
+
+                    ValuesListIds.push(
+
+                      { Display:n, IdArcher: S[SQ].RelatedValuesListId[n] }
+
+                    )
+
+                  } )
+
+              }
+
+
           })
         })
      } )
     });
 
 
+
+
+    
+    const FupForms = [];
+    const FupSectionsArr = [];
+    const FupMain = [];
+
+
+    cleaneddata.forEach( (el, index) => {
+
+      FupSectionsArr.push(
+
+        {
+          Name: el.Section, 
+          Active:true, 
+          Order: index+1
+        }
+
+      )
+
+      FupForms.push( {
+
+        Section: index+1, 
+        Active:true, 
+        IdsArcher: el.arrs.map(m =>{  
+
+          FupMain.push({
+
+            "IdArcher": m.IdArcher,
+            "Active": true,
+            "Order": 0,
+            "Vinculated": []
+          })
+          
+          return m.IdArcher }),
+      } );
+
+    } )
+    
         
     fs.writeFileSync("jsondata.json", JSON.stringify(cleaneddata), err => console.log(err));
     fs.writeFileSync("Subforms.json", JSON.stringify(Subforms), err => console.log(err));
     fs.writeFileSync("SubformsQuests.json", JSON.stringify(SubformsQuests), err => console.log(err));
     fs.writeFileSync("ValuesListIds.json", JSON.stringify(ValuesListIds), err => console.log(err));
     fs.writeFileSync("RelatedValuesListIdCleaned.json", JSON.stringify(RelatedValuesListIdCleaned), err => console.log(err));
-    fs.writeFileSync("AllQuestions.json", JSON.stringify(AllQuestions), err => console.log(err));
+    fs.writeFileSync("AllQuestions.json", JSON.stringify(AllQuestions.concat(SubformsQuests)), err => console.log(err));
     fs.writeFileSync("Sections.sql", Keys, err => console.log(err));
+
+    const Arrkeys =  AllQuestions.concat( SubformsQuests ).map( a => Object.keys( a ) ).flat();
+
+    console.log(FupForms, "==============");
+
+
+    let FupSections =  '';
+
+    FupSectionsArr.forEach( el => {
+
+
+      FupSections += `
+      INSERT INTO FupSections ( [Name], [Order] ) VALUES ( '${el.Section}', '${el.Order}' );
+      `;
+
+    } )
+
+   
+
+    if (!fs.existsSync(folderPath)) {
+      // Crea la carpeta si no existe
+      fs.mkdirSync(folderPath, { recursive: true }); // 'recursive: true' crea carpetas anidadas si es necesario
+      console.log('Carpeta creada:', folderPath);
+    } else {
+      console.log('La carpeta ya existe:', folderPath);
+    }
+
+
+    fs.mkdirSync(folderPath, { recursive: true });
+
 
 
 });
